310ce1150af9d93f3d7b07c1e8af8fa648b2d250
Eric Johnson <eric.j.johnson22@gmail.com>
2025-02-05 12:18:51 -0600

    Add more tests for sign extension

--- test/src/test_util.cpp
+++ test/src/test_util.cpp
@@ -5,8 +5,21 @@ TEST_GROUP(Util) {
 };
 
 TEST(Util, SignExtend) {
-    uint16_t value = 0x0011;
-    uint16_t old_value = value;
+    // Checks standard case, positive integer should not change bits
+    int16_t value = 0x00FF;
+    int16_t old_value = value;
     SIGN_EXTEND16(value, 9);
-    UNSIGNED_LONGS_EQUAL(value, old_value);
+    LONGS_EQUAL(old_value, value);
+
+    // Checks standard case, negative integer should change bits -1 in 9 bits -> -1 in 16
+    value = 0x01FF;
+    SIGN_EXTEND16(value, 9);
+    CHECK(value != old_value);
+    LONGS_EQUAL(-1, value);
+
+    // Checks pathalogical case where bits outside of bit width are set, should not change bits
+    value = 0x1001;
+    old_value = value;
+    SIGN_EXTEND16(value, 9);
+    LONGS_EQUAL(old_value, value);
 }
