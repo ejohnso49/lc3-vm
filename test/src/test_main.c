3a208b269a394839d9d8f42cd524b5488e212fb3
Eric Johnson <eric.j.johnson22@gmail.com>
2025-02-04 18:16:54 -0600

    Add tests w/ werk
    Using this to save progress before attempting to switch to cpputest

--- test/src/test_main.c
+++ test/src/test_main.c
@@ -1,14 +1,21 @@
 #include "unity.h"
+#include <stdio.h>
 
-void setUp(void) {}
-void tearDown(void) {}
+void setUp(void) {
+    printf("Hello regular setup\n");
+}
+void tearDown(void) {
+    printf("Hello regular teardown");
+}
 
-void test_StupidTest(void) {
-    TEST_ASSERT_EQUAL_HEX8(40, 40);
+void stupid_test(void) {
+    printf("Running stupid_test\n");
+    TEST_ASSERT_EQUAL_CHAR(0x1, 0x2);
 }
 
 int main(void) {
     UNITY_BEGIN();
-    RUN_TEST(test_StupidTest);
+    RUN_TEST(stupid_test);
+    RUN_TEST(stupid_test);
     return UNITY_END();
 }
