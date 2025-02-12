c35576d966003f600a85493662642a7af7e223b9
Eric Johnson <eric.j.johnson22@gmail.com>
2025-02-12 01:23:44 -0600

    Actually execute the instructions in processor_run :0)

--- src/processor.c
+++ src/processor.c
@@ -25,32 +25,46 @@ void processor_run(void) {
                 op_add(instruction);
                 break;
             case OP_LD:
+                op_ld(instruction);
                 break;
             case OP_ST:
+                op_st(instruction);
                 break;
             case OP_JSR:
+                op_jsr(instruction);
                 break;
             case OP_AND:
+                op_and(instruction);
                 break;
             case OP_LDR:
+                op_ldr(instruction);
                 break;
             case OP_STR:
+                op_str(instruction);
                 break;
             case OP_RTI:
+                op_rti(instruction);
                 break;
             case OP_NOT:
+                op_not(instruction);
                 break;
             case OP_LDI:
+                op_ldi(instruction);
                 break;
             case OP_STI:
+                op_sti(instruction);
                 break;
             case OP_JMP:
+                op_jmp(instruction);
                 break;
             case OP_RES:
+                op_res(instruction);
                 break;
             case OP_LEA:
+                op_lea(instruction);
                 break;
             case OP_TRAP:
+                op_trap(instruction);
                 break;
             case OP_MAX:
             default:
