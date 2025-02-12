6a41d4361577d97b02806a21eba7817a1467ad2f
Eric Johnson <eric.j.johnson22@gmail.com>
2025-02-12 01:23:26 -0600

    Add system setup functions from tutorial

--- src/main.c
+++ src/main.c
@@ -1,7 +1,11 @@
-#include <stdio.h>
 #include "registers.h"
 #include "memory.h"
+#include "processor.h"
 #include "file.h"
+#include "system.h"
+#include <stdio.h>
+#include <signal.h>
+#include <stdlib.h>
 
 int main(int argc, char **argv) {
     if (argc < 2) {
@@ -9,17 +13,21 @@ int main(int argc, char **argv) {
         exit(2);
     }
 
-    if (file_read_image(argv[1])) {
-        printf("Failed to load image: %s\n", argv[i]);
-        exit(1);
-    }
+    signal(SIGINT, handle_interrupt);
+    disable_input_buffering();
 
     memory_init();
     registers_init();
     registers_pc_init();
 
+    if (file_read_image(argv[1])) {
+        printf("Failed to load image: %s\n", argv[1]);
+        exit(1);
+    }
+
     printf("Starting LC3 Virtual Machine\n");
     processor_run();
+    restore_input_buffering();
 
     return 0;
 }
